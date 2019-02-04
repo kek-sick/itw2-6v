479595738c7f1ef55fd7e4f3c25b2e36eb8206b4
Ilya <link.s_tvink@mail.ru>
2019-02-04 23:56:08 +0300

    animation added

--- itw2-6v/itw2-6v.ino
+++ itw2-6v/itw2-6v.ino
@@ -9,6 +9,8 @@
 #define HIGH_VOLTAGE A1
 #define FILAMENT A2
 #define LIGHT_SENSOR A3
+#define WF_SECOND_TAP 600   //время окончания ожидания второго тапа
+#define WF_DEBOUNCE 300		//время окончания ожидания дребезга переключателя
 
 iarduino_RTC clock(RTC_DS1302, 10, 12, 11);
 
@@ -17,7 +19,7 @@ void lamp2();
 void show_s(byte, byte);
 //void show_r_strg(byte*);
 
-unsigned long time = 0, fst_tap_time = 0, show_time = 0, mills_ftt = 0;
+unsigned long time = 0, fst_tap_time = 0, GetTemp_time = 0, mills_ftt = 0;
 byte a = 0,b = 0;
 bool first_tap_btn_flag = false,	//if one tap happened
 	second_tap_btn_flag = false,	//if two taps hepptned
@@ -50,6 +52,11 @@ bool symb[][8] = {
 	{1,0,0,0,0,0,0,0}, // dots 18
 	{0,0,1,1,1,1,0,0}, // o (upper) 19
 };
+
+byte animation[2][11] = {
+	{0,0,3,5,4,2,6,8,7,1,0},
+	{0,0,7,5,8,6,2,4,3,1,0}
+};
 // the setup function runs once when you press reset or power the board
 void setup() {
 	pinMode(MESH1, OUTPUT);
@@ -69,7 +76,7 @@ void setup() {
 	
 	clock.begin();
 	//clock.settime(0,5, 2, 1, 2, 19, 5);
-	show_s(16, 16);    //say HI
+	show_s(16, 16);    //off all segments
 	//time = millis();
 	//Serial.begin(9600);
 	//Serial.println(clock.minutes);
@@ -88,18 +95,29 @@ void loop() {
 	/*Serial.print(first_tap_btn_flag);
 	Serial.print(allow_secon_tap);
 	Serial.println(second_tap_btn_flag);*/
+	if (mills_ftt < WF_SECOND_TAP)
+	{
+		if (mills_ftt % 10 < 5)
+		{
+			show_anim(mills_ftt / 60, 1);
+		}
+		else 
+		{
+			show_anim(mills_ftt / 60, 2);
+		}
+	}
 	//first tap detection
 	if (!digitalRead(13) && !first_tap_btn_flag)
 	{
 		first_tap_btn_flag = true;
 		fst_tap_time = millis();
-		show_time = fst_tap_time;
+		//show_time = fst_tap_time;
 		b = GetTemp();
 	}
 	mills_ftt = millis() - fst_tap_time;
 
 	//allowing to detect secont tap
-	if (digitalRead(13) && !allow_secon_tap && first_tap_btn_flag && mills_ftt >350 && mills_ftt < 600)
+	if (digitalRead(13) && !allow_secon_tap && first_tap_btn_flag && mills_ftt > WF_DEBOUNCE && mills_ftt < WF_SECOND_TAP)
 	{
 		allow_secon_tap = true;
 	}
@@ -119,10 +137,10 @@ void loop() {
 		digitalWrite(FILAMENT, HIGH);
 	}
 	//showing time
-	if (first_tap_btn_flag && !second_tap_btn_flag  && !has_shown && mills_ftt > 600)
+	if (first_tap_btn_flag && !second_tap_btn_flag  && !has_shown && mills_ftt > WF_SECOND_TAP)
 	{
 		a = clock.Hours;
-		while (millis() - show_time < 1500)
+		while (millis() - fst_tap_time < 1500)
 		{
 			show_s(byte(a/10),byte(a%10));
 		}
@@ -132,7 +150,7 @@ void loop() {
 		digitalWrite(1, LOW);
 		a = clock.minutes;
 		delay(300);
-		while (millis() - show_time < 2800)
+		while (millis() - fst_tap_time < 2800)
 		{
 			show_s(byte(a / 10), byte(a % 10));
 		}
@@ -142,13 +160,13 @@ void loop() {
 		allow_secon_tap = false;
 	}
 	//showing data
-	if (first_tap_btn_flag && second_tap_btn_flag && !has_shown && mills_ftt > 600)
+	if (first_tap_btn_flag && second_tap_btn_flag && !has_shown && mills_ftt > WF_SECOND_TAP)
 	{
-		while (millis() - show_time < 1500)
+		while (millis() - fst_tap_time < 1500)
 		{
 			show_s(10, 19);
 		}
-		while (millis() - show_time < 2800)
+		while (millis() - fst_tap_time < 2800)
 		{
 			show_s(byte(b / 10), byte(b % 10));
 		}
@@ -177,6 +195,25 @@ void lamp1() {
 void lamp2() {
 	digitalWrite(MESH1, HIGH);
 	digitalWrite(MESH2, LOW);
+}
+
+void show_anim(byte frame, byte lamp) {
+	byte seg[3];
+	if (lamp == 1) {
+		lamp1();
+	}
+	else
+	{
+		lamp2();
+	}
+	for (size_t i = 0; i < 3; i++)seg[i] = animation[lamp - 1][frame + i];  //обозначаем нужные сегменты в этом кадре
+	for (byte i = 2; i < 10; i++)		// вырубаем все сегменты
+	{
+		digitalWrite(i,HIGH);
+	}
+	if(seg[1]!=0){digitalWrite(seg[1]+1,LOW);}  //включаем нужные 3
+	if(seg[2]!=0){digitalWrite(seg[2]+1,LOW);}
+	if(seg[0]!=0){digitalWrite(seg[0]+1,LOW);}
 }
 
 void show_s(byte symbol_write_1, byte symbol_write_2) {
